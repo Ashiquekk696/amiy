61c5bccec0f1dc2681f6cf4c896ce2367ff9bc14
Ashiquekk696 <ashiquesofdevoloper@gmail.com>
2024-11-05 16:47:32 +0530

    'reupdated

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6e9f863d0492f0f28bab13c40e44f427
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "cb6d8dbf23f49667f65bbd481dbffa71",
+"flutter_bootstrap.js": "fa89d13d3e78b6137452ed8d720eb44f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
